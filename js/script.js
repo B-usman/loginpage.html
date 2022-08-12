7d17b7defd71baafef79244419cfcc3914866b39
B-usman <babatundeusman82@gmail.com>
2022-08-12 23:40:23 +0100

    edit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+ document.getElementById('normal').onclick = function(){
+   document.getElementById('css').href ="css/style.css"
+ }
+
+ document.getElementById('dark').onclick = function(){
+   document.getElementById('css').href ="css/dark.css"
+ }
+
+ document.getElementById('blue').onclick = function(){
+   document.getElementById('css').href ="css/blue.css"
+ }
+
+
+ 
---
+++ js/signup.js
@@ -0,0 +1,13 @@
+
+
+ document.getElementById('snormal').onclick = function(){
+    document.getElementById('cs2').href ="css/style.css"
+  }
+  
+  document.getElementById('sdark').onclick = function(){
+    document.getElementById('cs2').href ="css/dark.css"
+  }
+  
+  document.getElementById('sblue').onclick = function(){
+    document.getElementById('cs2').href ="css/blue.css"
+  }
