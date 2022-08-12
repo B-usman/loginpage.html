f87c6afe35275b7160476e3557a28baf34c4816f
B-usman <babatundeusman82@gmail.com>
2022-08-12 18:37:12 +0100

    page edited

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 document.getElementById('create').onclick = function(){
    document.getElementById('login').style. display ="none";
    document.getElementById('signup').style. display ="block"
@@ -7,4 +8,16 @@ document.getElementById('create').onclick = function(){
 document.getElementById('signin').onclick = function(){
     document.getElementById('login').style. display ="block";
     document.getElementById('signup').style. display ="none"
+ }
+
+ document.getElementById('normal').onclick = function(){
+   document.getElementById('css').href ="css/style.css"
+ }
+
+ document.getElementById('dark').onclick = function(){
+   document.getElementById('css').href ="css/dark.css"
+ }
+
+ document.getElementById('blue').onclick = function(){
+   document.getElementById('css').href ="css/blue.css"
  }
